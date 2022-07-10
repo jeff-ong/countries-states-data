1facd596daee5839f7b60e12947b6d66006732a3
jeff-ong <jeff-ong@outlook.com>
2022-07-10 12:26:19 -0400

    init

---
+++ dist/index.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const countries = require("./data/countries");
+const states = require("./data/states");
+
+function getAllCountries() {
+  return countries;
+}
+
+function getCountryByName(name) {
+    return countries.find(country => country.name === name);
+}
+
+function getStatesByCountryName(name) {
+  return states.find((state) => state.country_name === name);
+}
+
+function getStatesByCountryCode(code) {
+  return states.find((state) => state.country_code === code);
+}
+
+function getAllStates() {
+  return states;
+}
+
+module.exports = {
+  getAllCountries,
+  getCountryByName,
+  getStatesByCountryName,
+  getStatesByCountryCode,
+  getAllStates,
+};
